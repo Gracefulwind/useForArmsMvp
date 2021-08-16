659ac1b01aa7d832ddce1ed90a922bae5609772d
GitHub Action <action@github.com>
2021-08-16 08:26:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/gracefulwind/useforarmsmvp/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gracefulwind.useforarmsmvp.services
+
+import com.github.gracefulwind.useforarmsmvp.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/gracefulwind/useforarmsmvp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gracefulwind/useforarmsmvp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gracefulwind.useforarmsmvp.services
 
+import com.github.gracefulwind.useforarmsmvp.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
